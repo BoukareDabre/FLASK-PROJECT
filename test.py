7e4b6d73389933b7c46fe6cb034ef5b36fd88685
BoukareDabre <boukaredabre00bob@gmail.com>
2025-01-20 22:47:26 +0100

    aggiunta classe film

--- test.py
+++ test.py
@@ -8,7 +8,81 @@ app = Flask(__name__,template_folder=template_dir)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('base_film.html')
+    return render_template('Index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#mi creao la mia classe film
+class Film:
+    def __init__(
+        self,
+        id,
+        title,
+        director,
+        genre,
+        year,
+        actors,
+        release_date,
+        duration_minutes,
+        distribution,
+        synopsis,
+        trailer_url,
+        poster_url,
+        screenings,
+    ):
+        self.id = id
+        self.title = title
+        self.director = director
+        self.genre = genre
+        self.year = year
+        self.actors = actors
+        self.release_date = release_date
+        self.duration_minutes = duration_minutes
+        self.distribution = distribution
+        self.synopsis = synopsis
+        self.trailer_url = trailer_url
+        self.poster_url = poster_url
+        self.screenings = screenings
+
+
+    #aggiungo tutti i metodi get
+    def get_id(self):
+        return self.id
+
+    def get_title(self):
+        return self.title
+
+    def get_director(self):
+        return self.director
+
+    def get_genre(self):
+        return self.genre
+
+    def get_year(self):
+        return self.year
+
+    def get_actors(self):
+        return self.actors
+
+    def get_release_date(self):
+        return self.release_date
+
+    def get_duration_minutes(self):
+        return self.duration_minutes
+
+    def get_distribution(self):
+        return self.distribution
+
+    def get_synopsis(self):
+        return self.synopsis
+
+    def get_trailer_url(self):
+        return self.trailer_url
+
+    def get_poster_url(self):
+        return self.poster_url
+
+    def get_screenings(self):
+        return self.screenings
