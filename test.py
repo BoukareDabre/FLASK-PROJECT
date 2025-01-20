4986702a051e3ef5f1682946dabf3a4f5193a5d2
BoukareDabre <boukaredabre00bob@gmail.com>
2025-01-20 12:42:06 +0100

    modifiche storia luogo

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ app = Flask(__name__,template_folder=template_dir)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('story_cinema.html')
+    return render_template('luogo_cinema.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
